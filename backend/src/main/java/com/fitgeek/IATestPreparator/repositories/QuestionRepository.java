097db772634a350affd46874be2ba9894f243e2e
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-01-26 09:59:21 +0100

    Feat: Added QuestionRepository

---
+++ backend/src/main/java/com/fitgeek/IATestPreparator/repositories/QuestionRepository.java
@@ -0,0 +1,13 @@
+package com.fitgeek.IATestPreparator.repositories;
+
+import com.fitgeek.IATestPreparator.entities.Question;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface QuestionRepository extends JpaRepository<Question, Long> {
+
+    List<Question> findByQuizIdOrderByOrderIndexAsc(Long quizId);
+}
