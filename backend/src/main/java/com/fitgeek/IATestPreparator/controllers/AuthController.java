19707dff2ff94957ec9edb0d2c1d795d43daaea8
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-01-28 10:33:06 +0100

    Feat: Created class AuthController and successfully tested endpoints

---
+++ backend/src/main/java/com/fitgeek/IATestPreparator/controllers/AuthController.java
@@ -0,0 +1,35 @@
+package com.fitgeek.IATestPreparator.controllers;
+
+import com.fitgeek.IATestPreparator.dtos.LoginRequestDto;
+import com.fitgeek.IATestPreparator.dtos.LoginResponseDto;
+import com.fitgeek.IATestPreparator.dtos.RegisterRequestDto;
+import com.fitgeek.IATestPreparator.dtos.RegisterResponseDto;
+import com.fitgeek.IATestPreparator.services.AuthService;
+import jakarta.validation.Valid;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api/auth")
+@RequiredArgsConstructor
+public class AuthController {
+
+    private final AuthService authService;
+
+    @PostMapping("/register")
+    public ResponseEntity<?> register(@RequestBody @Valid RegisterRequestDto registerRequestDto) {
+        RegisterResponseDto responseDto = authService.register(registerRequestDto);
+        return ResponseEntity.status(HttpStatus.CREATED).body(responseDto);
+    }
+
+    @PostMapping("/login")
+    public ResponseEntity<?> login(@RequestBody @Valid LoginRequestDto loginRequestDto) {
+        LoginResponseDto responseDto = authService.login(loginRequestDto);
+        return ResponseEntity.ok(responseDto);
+    }
+}
