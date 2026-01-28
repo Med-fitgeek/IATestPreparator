fd8ea19a59851d9bc1fdf220e69dee2ad6858932
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-01-28 18:07:54 +0100

    Feat: Enabled pagination on methode findByOwnerId on DocumentRepository

--- backend/src/main/java/com/fitgeek/IATestPreparator/repositories/DocumentRepository.java
+++ backend/src/main/java/com/fitgeek/IATestPreparator/repositories/DocumentRepository.java
@@ -2,6 +2,8 @@ package com.fitgeek.IATestPreparator.repositories;
 
 import com.fitgeek.IATestPreparator.entities.Document;
 import com.fitgeek.IATestPreparator.entities.enums.Status;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -10,6 +12,6 @@ import java.util.Optional;
 @Repository
 public interface DocumentRepository extends JpaRepository<Document, Long> {
 
-    Optional<Document> findByOwnerId(Long owner_id);
+    Page<Document> findByOwnerId(Long owner_id, Pageable pageable);
     Optional<Document> findByStatus(Status status);
 }
