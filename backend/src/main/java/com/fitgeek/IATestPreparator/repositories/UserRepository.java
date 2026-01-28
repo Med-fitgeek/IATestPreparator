f2e6e8c8f41150f11d4b5aeedebd33326ba78cae
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-01-28 10:29:34 +0100

    Feat: Added method existsByUsername on UserRepository

--- backend/src/main/java/com/fitgeek/IATestPreparator/repositories/UserRepository.java
+++ backend/src/main/java/com/fitgeek/IATestPreparator/repositories/UserRepository.java
@@ -10,5 +10,7 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
 
     Optional<User> findByEmail(String username);
+    User findByUsername(String username);
     boolean existsByEmail(String email);
+    boolean existsByUsername(String username);
 }
