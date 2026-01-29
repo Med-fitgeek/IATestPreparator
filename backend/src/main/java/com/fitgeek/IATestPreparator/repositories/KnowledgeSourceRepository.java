bb7800f35d931c8e0b20ca75b7e4c9157f21ec38
Ndongo Medoune NDIAYE <ndongomedoune.ndiaye@gmail.com>
2026-01-29 15:37:24 +0100

    Feat : created KnowledgeSourceRepository

---
+++ backend/src/main/java/com/fitgeek/IATestPreparator/repositories/KnowledgeSourceRepository.java
@@ -0,0 +1,8 @@
+package com.fitgeek.IATestPreparator.repositories;
+
+import com.fitgeek.IATestPreparator.entities.KnowledgeSource;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface KnowledgeSourceRepository extends JpaRepository<KnowledgeSource, Integer> {
+
+}
